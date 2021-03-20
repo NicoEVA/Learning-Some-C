405719c6f95904ee44de188b232f7f5c36b88376
NicoEVA <nico.eins@web.de>
2021-03-20 20:09:34 +0100

    actually fucking fixed it

--- get_obstacles.c
+++ get_obstacles.c
@@ -68,9 +68,10 @@ int black_array[60][40] = {
 
 int obs_counter = 0;
 int obs_1[50][3]={0};
-int rewriter    = 0;
+int obs_2[50][3]={0};
+int rewriter,rewriter2    = 0;
 int i,j,p,pnew,o,count1;
-int minl,minr;
+int minl,maxr,cr=0;
 
 int main()
 {    
@@ -121,73 +122,97 @@ int main()
     
     }   
 
-    rewriter =0;
+    
 
-    for(i=50;i>0;i--){
+    for(i=0;i<50;i++){
         
         if(obs_1[i][0]!=0)
         {
             
-            for(j=i;j>0;j--)
+            for(j=0+i;j<50;j++)
             {
-                if(obs_1[j][0]!=0 && obs_1[j][0]<obs_1[i][0])
+                if(obs_1[j][0]>obs_1[i][0])
                 {
                     if (obs_1[j][1] >= obs_1[i][1]  && obs_1[j][1] <= obs_1[i][2]){
                         if(obs_1[j][2] > obs_1[i][2]){
-                            obs_1[rewriter][0] = obs_1[i][0];
-                            obs_1[rewriter][1] = obs_1[i][1];
-                            obs_1[rewriter][2] = obs_1[j][2];
-                            rewriter +=1;
-                            obs_1[j][0]=0;
-                            obs_1[j][1]=0;
-                            obs_1[j][2]=0;
-                        }else{
-                            obs_1[rewriter][0] = obs_1[i][0];
-                            obs_1[rewriter][1] = obs_1[i][1];
-                            obs_1[rewriter][2] = obs_1[i][2]; 
-                            rewriter +=1;
-                            obs_1[j][0]=0;
-                            obs_1[j][1]=0;
-                            obs_1[j][2]=0;
-                        }   
+                            maxr = obs_1[j][2];
+                            minl = obs_1[i][1];
+                        }
+                        else{
+                            maxr = obs_1[i][2];
+                            minl = obs_1[i][1];
+                        }
+                        cr = obs_1[j][0];   
+                    }
 
                     if (obs_1[j][2] >= obs_1[i][1]  && obs_1[j][2] <= obs_1[i][2]){
                         if(obs_1[j][1] < obs_1[i][1]){
-                            obs_1[rewriter][0] = obs_1[i][0];
-                            obs_1[rewriter][1] = obs_1[j][1];
-                            obs_1[rewriter][2] = obs_1[i][2];
-                            rewriter +=1;
-                            obs_1[j][0]=0;
-                            obs_1[j][1]=0;
-                            obs_1[j][2]=0;
-                        }else{
-                            obs_1[rewriter][0] = obs_1[i][0];
-                            obs_1[rewriter][1] = obs_1[i][1];
-                            obs_1[rewriter][2] = obs_1[i][2]; 
-                            rewriter +=1;
-                            obs_1[j][0]=0;
-                            obs_1[j][1]=0;
-                            obs_1[j][2]=0;
+                            maxr = obs_1[i][2];
+                            minl = obs_1[j][1];
+                        }
+                        else{
+                            maxr = obs_1[i][2];
+                            minl = obs_1[i][1];
                         }   
-
-                    }
+                        cr = obs_1[j][0];   
                     }
                     
+                    
+                    
+                }
+                //printf("current i %d, j %d, cr %d, minl %d, maxr %d\n",i,j,cr,minl,maxr);
+                
+            }
+            printf("O U T P U T i %d, j %d, cr %d, minl %d, maxr %d\n\n",i,j,cr,minl,maxr);
+            if(cr==0){
+                
+            }
+            else{
+                if(obs_2[rewriter2-1][0]== cr){
+                    
+                    if(obs_2[rewriter2-1][1]==minl && obs_2[rewriter2-1][2]==maxr){
+                        cr=0;
+                        minl=0;
+                        maxr=0; 
+
+                    }
+                    if(obs_2[rewriter2-1][1]<minl || obs_2[rewriter2-1][2]>maxr){
+                        cr=0;
+                        minl=0;
+                        maxr=0; 
+
+                    }
+                }
+                
+                else{
+                    obs_2[rewriter2][0]=cr;
+                    obs_2[rewriter2][1]=minl;
+                    obs_2[rewriter2][2]=maxr;
+                    rewriter2 +=1;
+                    cr=0;
+                    minl=0;
+                    maxr=0; 
                 }
 
                 
+
+            
             }
+        
 
         }
-         
+        
 
     }
 
+    // for(i=0;i<15;i++)
+    // {   
+    //     printf("obstacle %d %d %d \n",obs_1[i][0],obs_1[i][1],obs_1[i][2]);
+    // }  
 
-
-    for(i=0;i<50;i++)
-    {
-        printf("obstacle %d %d %d \n",obs_1[i][0],obs_1[i][1],obs_1[i][2]);
+    for(i=0;i<15;i++)
+    {   
+        printf("obstacle %d %d %d \n",obs_2[i][0],obs_2[i][1],obs_2[i][2]);
     }  
 
     return 0;
